efe9a4beabce5bed7acee47a167de838ebe789a9
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2025-01-25 09:32:41 +0200

    chore(fe): stub for hex storage

--- fe/src/coordinates.ts
+++ fe/src/coordinates.ts
@@ -1,5 +1,22 @@
 import * as THREE from "three";
 
+type CubeCoordStr = string;
+
+export class HexMap {
+  map: Map<CubeCoordStr, number>;
+
+  // TODO: callbacks when change happens, idk?
+
+  constructor() {
+    this.map = new Map<CubeCoordStr, number>();
+    this.map.set("0,0,0", 1);
+    this.map.set("1,-1,0", 1);
+    this.map.set("-1,1,0", 1);
+    this.map.set("0,1,-1", 1);
+    this.map.set("0,-1,1", 1);
+  }
+}
+
 export class CubeCoordinates {
   q: number;
   r: number;
@@ -10,6 +27,21 @@ export class CubeCoordinates {
     this.r = r;
     this.s = s;
   }
+
+  public to_string(): CubeCoordStr {
+    return `${this.q},${this.r},${this.s}`;
+  }
+
+  public static from_string(str: CubeCoordStr): CubeCoordinates {
+    const seg = str.split(",").map((e) => e.trim());
+    return new CubeCoordinates(
+      parseInt(seg[0], 10),
+      parseInt(seg[1], 10),
+      parseInt(seg[2], 10),
+    );
+  }
+
+  // Tostring methods
 
   public to_planar_unit(): THREE.Vector2 {
     // Increasing Q - (-Y, +X) (120deg counter-clock from screen down)
