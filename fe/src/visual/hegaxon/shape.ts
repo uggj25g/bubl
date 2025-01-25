c36d3f2c1182600a4f1228d62f012cade895d712
areglitis <andiseglitis@hackmotion.com>
2025-01-25 12:50:26 +0200

    fix(fe): shape building

--- fe/src/visual/hegaxon/shape.ts
+++ fe/src/visual/hegaxon/shape.ts
@@ -4,13 +4,11 @@ export class HexagonShape extends THREE.Shape {
   constructor() {
     super();
     const size = 0.5;
-    const centerX = 0;
-    const centerY = 0;
-    this.moveTo(centerX, centerY + size);
+    this.moveTo(0, 0 + size);
     for (let i = 1; i <= 6; i++) {
       this.lineTo(
-        centerX + size * Math.sin((i * Math.PI) / 3),
-        centerY + size * Math.cos((i * Math.PI) / 3)
+        size * Math.sin((i * Math.PI) / 3),
+        size * Math.cos((i * Math.PI) / 3)
       );
     }
   }
