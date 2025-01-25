336e2917385ecbcfe3bd15d1fb73e5e474147b3d
paulsnar <paulsnar@paulsnar.lv>
2025-01-25 13:48:32 +0200

    refactor(be): internal grid representation, player entity, deferred extent update

--- be/src/grid.ts
+++ be/src/grid.ts
@@ -2,10 +2,10 @@ import * as T from '../../types';
 
 const GRID_COMMIT_TICK_RATE = 100;
 const GRID_DECAY_TICK_RATE = 500;
+const GRID_VACUUM_RATE = 10000;
 const CELL_FILLED_MAX_AGE_SECONDS = 20;
 
 const CELL_FILLED_MAX_AGE_TICKS = CELL_FILLED_MAX_AGE_SECONDS * 1000 / GRID_DECAY_TICK_RATE;
-const CELL_DECAY_PER_TICK = 1 / CELL_FILLED_MAX_AGE_TICKS;
 
 export type CubeLocation = { q: T.Integer, r: T.Integer, s: T.Integer };
 export const cube = (q: T.Integer, r: T.Integer, s: T.Integer) => ({ q, r, s });
@@ -28,23 +28,54 @@ export const cube_is_adjacent = (a: CubeLocation, b: CubeLocation) => {
     return diffs[0] === -1 && diffs[1] === 0 && diffs[2] === 1;
 };
 
-// type Cell = CellTrail | CellFilled;
-// type CellTrail = {
-//     state: T.CellState.TRAIL,
-//     ownerPlayerId: T.Integer,
-//     color: T.Integer,
+type GCellGrid = Record<T.CubeLocation, GCell>;
+type GCell = GCellBlank | GCellTrail | GCellFilled;
+type GCellBlank = {
+    location: CubeLocation,
+    state: T.CellState.BLANK,
+};
+type GCellTrail = {
+    location: CubeLocation,
+    state: T.CellState.TRAIL,
+    ownerPlayerId: T.Integer,
+    color: T.Integer,
 
-// };
-// type CellFilled = {
-//     state: T.CellState.FILLED,
-//     color: T.Integer,
+};
+type GCellFilled = {
+    location: CubeLocation,
+    state: T.CellState.FILLED,
+    color: T.Integer,
 
-//     /// number of DECAY ticks remaining
-//     age: number,
-// };
+    /// number of DECAY ticks remaining
+    age: number,
+};
 
-type Queue = T.CellGrid & { _count: number };
-const newQueue = (): Queue => {
+function gcellToTcell(cell: GCell): T.Cell {
+    switch (cell.state) {
+    case T.CellState.BLANK: {
+        return cell;
+    }
+    case T.CellState.FILLED: {
+        return {
+            state: cell.state,
+            color: cell.color,
+            age: cell.age / CELL_FILLED_MAX_AGE_TICKS,
+        };
+    }
+    case T.CellState.TRAIL: {
+        return {
+            state: cell.state,
+            color: cell.color,
+            age: 1, // TODO[paulsn]
+            ownerPlayer: cell.ownerPlayerId,
+        };
+    }
+    }
+}
+
+
+type GQueue = GCellGrid & { _count: number };
+const newQueue = (): GQueue => {
     let queue = Object.create(null);
     Object.defineProperty(queue, '_count', {
         enumerable: false,
@@ -54,54 +85,135 @@ const newQueue = (): Queue => {
     return queue;
 }
 
+type GExtent = {
+    q: [min: T.Integer, max: T.Integer],
+    r: [min: T.Integer, max: T.Integer],
+    s: [min: T.Integer, max: T.Integer],
+    toString(): string;
+};
+interface GExtentCalc {
+    extend(pos: CubeLocation): void;
+    finalize(): GExtent;
+}
+const newExtent = (): GExtentCalc => {
+    let any = false;
+    let ext: GExtent = {
+        q: [0, 0],
+        r: [0, 0],
+        s: [0, 0],
+        toString() {
+            return `q:${this.q[0]}..${this.q[1]}`
+                + ` r:${this.r[0]}..${this.r[1]}`
+                + ` s:${this.s[0]}..${this.s[1]}`;
+        },
+    };
+
+    const extend = (pos: CubeLocation) => {
+        if ( ! any) {
+            ext.q = [pos.q, pos.q];
+            ext.r = [pos.r, pos.r];
+            ext.s = [pos.s, pos.s];
+            any = true;
+            return;
+        }
+
+        ext.q[0] = Math.min(ext.q[0], pos.q);
+        ext.q[1] = Math.max(ext.q[1], pos.q);
+        ext.r[0] = Math.min(ext.r[0], pos.r);
+        ext.r[1] = Math.max(ext.r[1], pos.r);
+        ext.s[0] = Math.min(ext.s[0], pos.s);
+        ext.s[1] = Math.max(ext.s[1], pos.s);
+    };
+
+    const finalize = () => ext;
+
+    return { extend, finalize };
+}
+
 export class Grid {
-    cells: T.CellGrid;
-    queue: Queue;
+    #cells: GCellGrid;
+    #queue: GQueue;
+    #extent: GExtent;
+    filled: Set<T.CubeLocation>;
 
     onupdate?: (diff: T.CellGrid) => void;
 
-    filled: Set<T.CubeLocation>;
-
     constructor() {
-        this.cells = Object.create(null);
-        this.queue = newQueue();
-        this.onupdate = undefined;
-
+        this.#cells = Object.create(null);
+        this.#queue = newQueue();
+        this.#extent = newExtent().finalize();
         this.filled = new Set();
     }
 
     get cellGrid(): T.CellGrid {
-        return this.cells;
+        let grid: T.CellGrid = Object.create(null);
+
+        for (let [locationKey, gcell] of Object.entries(this.#cells)) {
+            let location = locationKey as T.CubeLocation;
+            grid[location] = gcellToTcell(gcell);
+        }
+
+        return grid;
     }
 
-    set(location: T.CubeLocation, cell: T.Cell) {
-        this.queue[location] = cell;
-        this.queue._count += 1;
+    get extent() {
+        return this.#extent;
+    }
+
+    setBlank(location: T.CubeLocation) {
+        this.#queue[location] = {
+            state: T.CellState.BLANK,
+            location: str_cube(location),
+        };
+        this.#queue._count += 1;
+    }
+
+    setFilled(location: T.CubeLocation, color: T.Integer) {
+        this.#queue[location] = {
+            state: T.CellState.FILLED,
+            location: str_cube(location),
+            color: color,
+            age: CELL_FILLED_MAX_AGE_TICKS,
+        };
+        this.#queue._count += 1;
+    }
+
+    setTrail(location: T.CubeLocation, color: T.Integer, ownerPlayerId: T.Integer) {
+        this.#queue[location] = {
+            state: T.CellState.TRAIL,
+            location: str_cube(location),
+            color: color,
+            ownerPlayerId: ownerPlayerId,
+        };
+        this.#queue._count += 1;
     }
 
     commit() {
-        if (this.queue._count === 0) return;
+        if (this.#queue._count === 0) return;
 
-        for (let [locationKey, cell] of Object.entries(this.queue)) {
+        let updated = Object.create(null) as T.CellGrid;
+        for (let [locationKey, cell] of Object.entries(this.#queue)) {
             if (typeof cell === 'number') continue;
             let location = locationKey as T.CubeLocation;
 
             if (cell.state === T.CellState.FILLED) {
                 this.filled.add(location);
-            } else if ((location in this.cells) && this.cells[location].state === T.CellState.FILLED) {
+            } else if (this.filled.has(location)) {
                 this.filled.delete(location);
             }
 
             if (cell.state === T.CellState.BLANK) {
-                delete this.cells[location];
+                delete this.#cells[location];
             } else {
-                this.cells[location] = cell;
+                this.#cells[location] = cell;
             }
+
+            updated[location] = gcellToTcell(cell);
 
         }
 
-        this.onupdate?.(this.queue);
-        this.queue = newQueue();
+        this.onupdate?.(updated);
+        this.#queue = newQueue();
     }
 
     decay() {
@@ -109,25 +221,27 @@ export class Grid {
 
         let touched = Object.create(null) as T.CellGrid;
         for (let location of this.filled) {
-            let cell = this.cells[location] as T.CellFilled;
-            cell.age -= CELL_DECAY_PER_TICK;
+            let cell = this.#cells[location] as GCellFilled;
+            cell.age -= 1;
             if (cell.age <= 0) {
-                touched[location] = {
-                    state: T.CellState.BLANK,
-                } as T.CellBlank;
-                delete this.cells[location];
+                touched[location] = { state: T.CellState.BLANK };
+                delete this.#cells[location];
                 this.filled.delete(location);
             } else {
-                touched[location] = cell;
-                // touched[location] = this.cells[location] = {
-                //     state: T.CellState.FILLED,
-                //     color: cell.color,
-                //     age: cell.age,
-                // } as T.CellFilled;
+                touched[location] = gcellToTcell(cell);
             }
         }
 
         this.onupdate?.(touched);
+    }
+
+    vacuum() {
+        let extent = newExtent();
+        for (let gcell of Object.values(this.#cells)) {
+            extent.extend(gcell.location);
+        }
+        this.#extent = extent.finalize();
+        console.log('[grid] updated extent: %s', this.#extent);
     }
 }
 
@@ -140,5 +254,9 @@ setInterval(() => {
 setInterval(() => {
     grid.decay();
 }, GRID_DECAY_TICK_RATE);
+
+setInterval(() => {
+    grid.vacuum();
+}, GRID_VACUUM_RATE);
 
 export default grid;
---
+++ be/src/player.ts
@@ -0,0 +1,144 @@
+import { type WebSocket } from 'ws';
+import * as T from '../../types';
+
+import GRID from './grid';
+import { send } from './index';
+
+export class Player {
+    id: T.PlayerState["id"];
+    conn: WebSocket;
+    state: T.PlayerState;
+
+    constructor(conn: WebSocket, state: T.PlayerState) {
+        this.conn = conn;
+        this.state = state;
+        this.id = state.id;
+
+        this.handleMessageBase = this.handleMessageBase.bind(this);
+        this.conn.on('message', this.handleMessageBase);
+    }
+    dispose() {
+        this.conn.removeListener('message', this.handleMessageBase);
+    }
+
+    handleMessageBase(rawMsg: any, isBinary: boolean) {
+        if (isBinary) return;
+        let strMsg = rawMsg.toString('utf8');
+        let anyMsg: any;
+        try {
+            anyMsg = JSON.parse(strMsg);
+        } catch (e) {
+            return;
+        }
+        if ( ! Array.isArray(anyMsg)) return;
+        if (anyMsg.length !== 2) return;
+        this.handleMessage(anyMsg as T.ClientMessage);
+    }
+
+    handleMessage(msg: T.ClientMessage) {
+        console.log('[%d msg] %o', this.id, msg);
+        switch (msg[0]) {
+        case T.MessageType.MOVE: return this.handleMove(msg[1]);
+        }
+    }
+    handleMove(msg: T.MoveMessage) {
+        const { location } = msg;
+        GRID.setFilled(this.state.location, this.state.color);
+
+        this.state.location = location;
+        GRID.setTrail(location, this.state.color, this.id);
+
+        PLAYERS.broadcastPlayerUpdate();
+    }
+}
+
+class Players {
+    #byWs: Map<WebSocket, Player>;
+    #byId: Map<T.PlayerState["id"], Player>;
+
+    constructor() {
+        this.#byWs = new Map();
+        this.#byId = new Map();
+
+        GRID.onupdate = (diff) => {
+            console.log('[grid] update', diff);
+            this.broadcastGridUpdate(diff);
+        };
+    }
+
+    #id(): T.PlayerState["id"] {
+        const scale = 1 << 31;
+        let id;
+        do {
+            id = Math.abs(Math.random() * scale) | 0;
+        } while (this.#byId.has(id));
+        return id;
+    }
+
+    spawn(conn: WebSocket): Player {
+        let id = this.#id();
+        let state: T.PlayerState = {
+            id: id,
+            color: 0, // TODO[paulsn] assign random
+            location: '0,0,0', // TODO[paulsn] assign random
+        };
+
+        let player = new Player(conn, state);
+        this.#byWs.set(conn, player);
+        this.#byId.set(id, player);
+
+        console.log('[%d connect]', id);
+
+        conn.once('close', () => {
+            this.#byWs.delete(conn);
+            this.#byId.delete(id);
+            this.broadcastPlayerUpdate();
+            console.log('[%d disconnect]', id);
+            player.dispose();
+        });
+
+        let otherPlayers = Array.from(this.#byId.values())
+            .filter(p => p.id !== player.id)
+            .map(p => p.state);
+
+        send(player.conn, [
+            T.MessageType.INIT,
+            {
+                protocolVersion: T.PROTOCOL_VERSION,
+                self: player.state,
+                others: otherPlayers,
+                grid: T.compressGrid(GRID.cellGrid),
+            },
+        ]);
+
+        this.broadcastPlayerUpdate(new Set([player]));
+
+        return player;
+    }
+
+    broadcastPlayerUpdate(except?: Set<Player>) {
+        except ??= new Set();
+
+        const msg: T.UpdateMessage = {
+            players: Array.from(this.#byId.values()).map(p => p.state),
+            gridDiff: [],
+        };
+
+        for (let [ws, player] of this.#byWs.entries()) {
+            if (except.has(player)) continue;
+            send(ws, [T.MessageType.UPDATE, msg]);
+        }
+    }
+
+    broadcastGridUpdate(diff: T.CellGrid) {
+        const msg: T.UpdateMessage = {
+            players: Array.from(this.#byId.values()).map(p => p.state),
+            gridDiff: T.compressGrid(diff),
+        };
+        for (let ws of this.#byWs.keys()) {
+            send(ws, [T.MessageType.UPDATE, msg]);
+        }
+    }
+}
+
+export const PLAYERS = new Players();
